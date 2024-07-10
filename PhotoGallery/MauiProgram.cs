c02e4b70a0e53848fdd02f9680ff869aee87d976
Guy Siedes <3strategy@gmail.com>
2024-07-10 07:57:52 +0300

    Added Material Symbols and changed up button to relevant icon.

--- PhotoGallery/MauiProgram.cs
+++ PhotoGallery/MauiProgram.cs
@@ -13,6 +13,7 @@ namespace PhotoGallery
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("MaterialSymbolsOutlined-VariableFont.ttf", "MaterialSymbols");
                 });
 
 #if DEBUG
