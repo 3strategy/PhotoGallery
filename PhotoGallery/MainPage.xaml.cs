c40fb777cebf105108c0270734a8b8eecc55663b
Guy Siedes <3strategy@gmail.com>
2024-07-10 08:43:25 +0300

    added the click events - round robin code changing hte image source of ChangingCarImg

--- PhotoGallery/MainPage.xaml.cs
+++ PhotoGallery/MainPage.xaml.cs
@@ -2,14 +2,23 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
+        int ind = 0;
+        string[] images = { "dotnet_bot.png", "dotnet_bot2.png" , "dotnet_bot3.png" , "dotnet_bot4.png" };
         public MainPage()
         {
             InitializeComponent();
         }
 
+        private void OnUpBtnClicked(object sender, EventArgs e)
+        {
+            ind = (ind - 1 + 4) % 4; // overcome % modulus discrepency.
+            ChangingCarImg.Source = images[ind];
+        }
 
+        private void OnDownBtnClicked(object sender, EventArgs e)
+        {
+            ChangingCarImg.Source = images[++ind % 4];
+        }
     }
 
 }
