2c85f4f6320be8ee275f8c460dd0f1255930346a
Guy Siedes <3strategy@gmail.com>
2024-07-10 10:14:55 +0300

    made Ind a full property so that hadnling across buttons is consistent and extendable to support state load from db or multiplayer signalR changes etc. Hopefull a correct approach.

--- PhotoGallery/MainPage.xaml.cs
+++ PhotoGallery/MainPage.xaml.cs
@@ -2,8 +2,33 @@
 {
     public partial class MainPage : ContentPage
     {
-        int ind = 0;
-        string[] images = { "dotnet_bot.png", "dotnet_bot2.png" , "dotnet_bot3.png" , "dotnet_bot4.png" };
+        private int ind;
+
+        public int Ind
+        {
+            get { return ind; }
+            set
+            {   // link UI logic to the lowest common denominator which is Ind property
+                if (value >= 0 && value < lenImg)
+                {
+                    if (value > ind)
+                        UpBtn2.IsEnabled = true;
+                    else if (value < ind)
+                        DownBtn2.IsEnabled = true;
+
+                    if (value == 0)
+                        UpBtn2.IsEnabled = false;
+                    else if (value == lenImg - 1)
+                        DownBtn2.IsEnabled = false;
+
+                    ind = value;
+                    ChangingCarImg.Source = images[ind];
+                }
+                // in all other cases ignoring the set.
+            }
+        }
+
+        string[] images = { "dotnet_bot.png", "dotnet_bot2.png", "dotnet_bot3.png", "dotnet_bot4.png" };
         int lenImg;
         public MainPage()
         {
@@ -11,40 +36,20 @@
             lenImg = images.Length;
         }
 
-        private void OnUpBtnClicked(object sender, EventArgs e)
-        {
-            ind = (ind - 1 + lenImg) % lenImg; // overcome % modulus discrepency.
-            ChangingCarImg.Source = images[ind];
-        }
+        private void OnUpBtnClicked(object sender, EventArgs e) => 
+            Ind = (Ind - 1 + lenImg) % lenImg; // overcome % modulus discrepency.
 
-        private void OnDownBtnClicked(object sender, EventArgs e)
-        {
-            ChangingCarImg.Source = images[++ind % lenImg];
-        }
+        private void OnDownBtnClicked(object sender, EventArgs e) => Ind = (Ind + 1) % lenImg;
 
         private void OnUpBtn2Clicked(object sender, EventArgs e)
         {
-            if(ind >0)
-            {  
-                OnUpBtnClicked(sender, e); // This implementation relies on the existance 
-                // of the side by side buttons. A stand alone code would be a bit differnt.
-                if(ind ==0)
-                    UpBtn2.IsEnabled = false;
-            }
-            DownBtn2.IsEnabled = true;
-
+            OnUpBtnClicked(sender, e); // This implementation relies on the existance 
+                                       // of the side by side buttons. A stand alone code would be a bit differnt.
         }
 
         private void OnDownBtn2Clicked(object sender, EventArgs e)
         {
-            if (ind < lenImg-1)
-            {
-                OnDownBtnClicked(sender, e); // This implementation relies on the existance 
-                // of the side by side buttons. A stand alone code would be a bit differnt.
-                if (ind == lenImg-1)
-                    DownBtn2.IsEnabled = false;
-            }
-            UpBtn2.IsEnabled = true;
+            OnDownBtnClicked(sender, e);
         }
     }
 
