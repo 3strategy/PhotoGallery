8aa35d23edc80464c87d48f88747ef65f74a1ec5
Guy Siedes <3strategy@gmail.com>
2024-08-05 09:20:32 +0300

    added all controls to RegistrationPage. Added validation to User class.

--- PhotoGallery/Models/User.cs
+++ PhotoGallery/Models/User.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
 
 namespace PhotoGallery.Models
 {
@@ -60,5 +63,44 @@ namespace PhotoGallery.Models
 
         public int Age => (DateTime.Now.Year - BirthDate.Year - (DateTime.Now.DayOfYear < BirthDate.DayOfYear ? 1 : 0));
 
+        public ICommand RegisterCommand => new Command(OnRegister);
+
+        private void OnRegister()
+        {
+            ErrorMessage = "";
+            if (UserName is null)
+                ErrorMessage += "חייבים שם משתמש\n";
+            else if (Regex.IsMatch(UserName, @"^\d") || UserName.Contains(" "))
+                ErrorMessage += "שם המשתמש לא יכול להתחיל בספרה ואסור שיכיל רווחים\n";
+
+            if(Password is null)
+                ErrorMessage += "חייבים סיסמא\n";
+            else if (!Regex.IsMatch(Password, @"^(?=.*[A-Z])(?=.*\d).+$"))
+                ErrorMessage += "סיסמה חייבת להכיל לפחות אות גדולה אחת ומספר\n";
+
+            if (Age < 18)
+                ErrorMessage += "הגיל חייב להיות גדול מ-18\n";
+
+            if (Email is null)
+                ErrorMessage += "חייבים מייל\n";
+            else if (!IsValidEmail(email))
+                ErrorMessage += "מייל לא תקין\n";
+
+            if(ErrorMessage =="")
+                ErrorMessage = "Registration successful!";
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
